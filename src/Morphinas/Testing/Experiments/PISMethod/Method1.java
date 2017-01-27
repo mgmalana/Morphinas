72c42ace139c5fe18df88cb6ee2df31559bf9c97
Laurenz <laurenztino@yahoo.com>
2017-01-27 20:15:51 +0800

    [JAN 27 - 8:15PM] Daily Push
    CHANGES:
    - Started PIS Method

--- src/Morphinas/Testing/Experiments/PISMethod/Method1.java
+++ src/Morphinas/Testing/Experiments/PISMethod/Method1.java
@@ -7,21 +7,65 @@ package Morphinas.Testing.Experiments.PISMethod;
  */
 public class Method1
 {
+
+	String[] storedPrefixes = { "pinag", "ka" };
+	String[] storedInfixes 	= { "in", "um" };
+	String[] storedSuffixes = { "in", "an" };
+
 	public Method1()
 	{
-
+		println("Running Method1...");
 	}
 
 	public static void main(String[] args)
 	{
 		String test = "pinagkainan";
 		Method1 m1 = new Method1();
+
+		println( m1.rmvPrefix(test) );
 	}
 
 	public String rmvPrefix(String stem)
 	{
 		String result = stem;
+		String pfxCompare;
+		int pfxLen;
+
+		for ( String prefix: storedPrefixes)
+		{
+			pfxLen = prefix.length();
+			pfxCompare = stem.substring(0, pfxLen);
+
+			if( pfxCompare.equalsIgnoreCase(prefix) )
+			{
+				return stem.substring(pfxLen);
+			}
+		}
 
 		return result;
+	}
+
+	public String rmvSuffix(String stem)
+	{
+		String result = stem;
+
+		for( String suffix: storedSuffixes)
+		{
+
+		}
+
+		return result;
+	}
+
+	public String rmvInfix(String stem)
+	{
+		String result = stem;
+
+		return result;
+	}
+
+	public static void println(Object text)
+	{
+		System.out.println(text.toString());
 	}
 }
