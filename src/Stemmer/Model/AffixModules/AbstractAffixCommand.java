616af6a75e3fc0d7016e8d256cdae8e266da6c14
Laurenz <laurenztino@yahoo.com>
2017-02-13 15:27:29 +0800

    [FEB 9 - 4:50PM] AffixCommand
    CHANGES:
    - AffixCommand for AffixModules

---
+++ src/Stemmer/Model/AffixModules/AbstractAffixCommand.java
@@ -0,0 +1,7 @@
+package Stemmer.Model.AffixModules;
+
+/**
+ * Created by laurenz on 09/02/2017.
+ */
+public abstract class AbstractAffixCommand {
+}
--- src/Stemmer/Model/AffixModules/Prefix/PrefixCommand.java
+++ src/Stemmer/Model/AffixModules/Prefix/PrefixCommand.java
@@ -9,6 +9,30 @@ import static Utility.print.println;
  */
 public class PrefixCommand
 {
+	String word;
+	public PrefixCommand(){}
+
+	public PrefixCommand(String word)
+	{
+		this.word = word;
+		performStemmingModules(word);
+	}
+
+	public String performStemmingModules(String word)
+	{
+		RemoveCommonPrefix rcp = new RemoveCommonPrefix();
+		this.word = rcp.reduceStem(word);
+		return word;
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+
 	public static class test
 	{
 		public static void main(String[] args)
