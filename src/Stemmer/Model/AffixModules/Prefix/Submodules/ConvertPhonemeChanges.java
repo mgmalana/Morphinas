42c89e070d2f790d9c9ea0c12c8c87ac286cd7c9
Laurenz <laurenztino@yahoo.com>
2017-03-02 19:00:43 +0800

    [MAR 01 - 9:51 PM] Added new ConvertPhonemeChanges
    CHANGES:
    - Please start this one
    - add more tests to Reduplication(Full and Partial)

---
+++ src/Stemmer/Model/AffixModules/Prefix/Submodules/ConvertPhonemeChanges.java
@@ -0,0 +1,7 @@
+package Stemmer.Model.AffixModules.Prefix.Submodules;
+
+/**
+ * Created by laurenz on 02/03/2017.
+ */
+public class ConvertPhonemeChanges {
+}
---
+++ src/Stemmer/Model/AffixModules/Suffix/Submodules/ConvertPhonemeChanges.java
@@ -0,0 +1,7 @@
+package Stemmer.Model.AffixModules.Suffix.Submodules;
+
+/**
+ * Created by laurenz on 02/03/2017.
+ */
+public class ConvertPhonemeChanges {
+}
